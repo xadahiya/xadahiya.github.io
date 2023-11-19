c5151dcd4f440d0dadadfe58c5d8955cc4451203
Akshay Dahiya <akshay@powerloom.io>
2023-11-19 23:27:47 +0530

    chore: update contents, initial build done

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,11 +1,3 @@
-/**
-*	Luique - Personal Portfolio HTML Template
-*	Version: 1.0
-*	Author: bslthemes
-*	Author URL: http://themeforest.net/user/bslthemes
-*	Copyright © Luique by bslthemes. All Rights Reserved.
-**/
-
 ( function( $ ) {
 	'use strict';
 
